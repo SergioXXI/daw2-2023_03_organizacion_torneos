9bdc15dc91dea9edbbaec0cc944c55dca70cbf23
OscarMatillaVara <oscarMatVar@usal.es>
2024-01-06 16:58:50 +0100

    Formulario torneo
    Funcionalidad basica

--- basic/views/site/form_torneo.php
+++ basic/views/site/form_torneo.php
@@ -7,40 +7,42 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'nombre') ?>
 
     <?= $form->field($model, 'descripcion') ?>
+
+    <?= $form->field($model, 'participantes_max') ?>
     
     <?php
+        echo '<br>';
+        // Sample data for dropdown options
+        $lista_disciplina = [
+            '1' => 'Futbol',
+            '2' => 'Baloncesto',
+            '3' => 'Tenis',
+        ];
 
-    // Sample data for dropdown options
-    $disciplina_id = [
-        '1' => 'Futbol',
-        '2' => 'Baloncesto',
-        '3' => 'Tenis',
-    ];
+        // Generating the dropdown list
+        echo $form->field($model, 'disciplina_id')->dropDownList($lista_disciplina, ['prompt' => 'Select Option']);
+        echo '<br><br>';
+        $lista_tipo_torneo = [
+            '1' => 'Eliminatorias',
+            '2' => 'Liga',
+            '3' => 'Amistoso',
+        ];
+        
 
-    // Generating the dropdown list
-    echo Html::dropDownList('dropdownName', null, $disciplina_id, ['prompt' => 'Select Option']);
-    echo '<br><br>';
-    $tipo_torneo_id = [
-        '1' => 'Eliminatorias',
-        '2' => 'Liga',
-        '3' => 'Amistoso',
-    ];
+        // Generating the dropdown list
+        echo $form->field($model, 'tipo_torneo_id')->dropDownList($lista_tipo_torneo, ['prompt' => 'Select Option']);
+        echo '<br><br>';
+        $lista_clase = [
+            '1' => 'Nacional',
+            '2' => 'Local',
+            '3' => 'Intenacional',
+        ];
+
+        // Generating the dropdown list
+        echo $form->field($model, 'clase_id')->dropDownList($lista_clase, ['prompt' => 'Select Option']);
+        echo '<br><br>';
     
-
-    // Generating the dropdown list
-    echo Html::dropDownList('dropdownName', null, $tipo_torneo_id, ['prompt' => 'Select Option']);
-    echo '<br><br>';
-    $clase_id = [
-        '1' => 'Nacional',
-        '2' => 'Local',
-        '3' => 'Intenacional',
-    ];
-
-    // Generating the dropdown list
-    echo Html::dropDownList('dropdownName', null, $clase_id, ['prompt' => 'Select Option']);
-    echo '<br><br>';
-    
-?>
+    ?>
 
 
     <div class="form-group">
