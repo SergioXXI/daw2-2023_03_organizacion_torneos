28ba66672a9b534cb09e2d7d1071b4f7a48c2a1f
JorgeApSa <id00800819@usal.es>
2024-01-09 13:49:20 +0100

    Modificacion de Archivos para la creacion de formulario de Login y Logout

--- basic/models/LoginForm.php
+++ basic/models/LoginForm.php
@@ -5,77 +5,34 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 
-/**
- * LoginForm is the model behind the login form.
- *
- * @property-read User|null $user
- *
- */
 class LoginForm extends Model
 {
-    public $username;
+    public $email;
     public $password;
-    public $rememberMe = true;
 
-    private $_user = false;
-
-
-    /**
-     * @return array the validation rules.
-     */
     public function rules()
     {
         return [
-            // username and password are both required
-            [['username', 'password'], 'required'],
-            // rememberMe must be a boolean value
-            ['rememberMe', 'boolean'],
-            // password is validated by validatePassword()
-            ['password', 'validatePassword'],
+            [['email', 'password'], 'required'],
+            ['email', 'email'],
         ];
     }
 
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
-     *
-     * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
-     */
-    public function validatePassword($attribute, $params)
-    {
-        if (!$this->hasErrors()) {
-            $user = $this->getUser();
-
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
-            }
-        }
-    }
-
-    /**
-     * Logs in a user using the provided username and password.
-     * @return bool whether the user is logged in successfully
-     */
     public function login()
     {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+        $user = User::findOne(['email' => $this->email]);
+
+        if (!$user) {
+            $this->addError('email', 'Email incorrecto.');
+        } elseif (!$user->validatePassword($this->password)) {
+            $this->addError('password', 'Contraseña incorrecta.');
+        } else {
+            Yii::$app->user->login($user);
+            return true;
         }
+
         return false;
     }
 
-    /**
-     * Finds user by [[username]]
-     *
-     * @return User|null
-     */
-    public function getUser()
-    {
-        if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
-        }
+}    
 
-        return $this->_user;
-    }
-}
--- basic/models/User.php
+++ basic/models/User.php
@@ -1,104 +1,64 @@
 <?php
 
 namespace app\models;
+use Yii;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    public function rules()
+    {
+        return [
+            [['id', 'nombre', 'apellido1', 'apellido2', 'email', 'password'], 'required'],
+            [['id', 'rol_id'], 'integer', 'max' => PHP_INT_MAX],
+            [['nombre', 'apellido1', 'apellido2', 'password'], 'string', 'max' => 100],
+            [['email'], 'string', 'max' => 100],
+            [['email'], 'unique'],
+        ];
+    }
+
     public $authKey;
     public $accessToken;
+    public $username;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
-    /**
-     * {@inheritdoc}
-     */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return static::findOne($id);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
+        // Implementa la lógica para encontrar un usuario por su accessToken aquí
         return null;
     }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['email' => $username]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getAuthKey()
     {
         return $this->authKey;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
+        return $this->getAuthKey() === $authKey;
     }
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
     public function validatePassword($password)
     {
         return $this->password === $password;
     }
 }
+
