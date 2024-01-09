f32ded568dd3f7cc1f16b4ca5940fde71b576f96
Manuel <manusanvicente15@gmail.com>
2024-01-10 00:16:06 +0100

    Añadido User, modelo, controller y vistas. Falta pulir roles

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -42,13 +42,14 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'User', 'url' => ['/user']],
             ['label' => 'Test', 'url' => ['/test/index']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
                     . Html::beginForm(['/site/logout'])
                     . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        'Logout (' . Yii::$app->user->identity->email . ')',
                         ['class' => 'nav-link btn btn-link logout']
                     )
                     . Html::endForm()
