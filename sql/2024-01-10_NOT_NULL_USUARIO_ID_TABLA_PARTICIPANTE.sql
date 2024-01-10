740b662e50a595e01c9bd946f4419a9c79997a05
diegoefe02 <difermo-02@hotmail.com>
2024-01-10 17:55:38 +0100

    Añadido archivo con codigo sql que añade en la tabla participante valores en el usuario_id y lo modifica a NOT NULL

---
+++ sql/2024-01-10_NOT_NULL_USUARIO_ID_TABLA_PARTICIPANTE.sql
@@ -0,0 +1,8 @@
+
+-- añade fecha fin a la tabla torneos
+UPDATE `participante` SET `usuario_id` = '1' WHERE `participante`.`id` = 1;
+UPDATE `participante` SET `usuario_id` = '2' WHERE `participante`.`id` = 2;
+UPDATE `participante` SET `usuario_id` = '3' WHERE `participante`.`id` = 3;
+
+ALTER TABLE participante
+MODIFY COLUMN usuario_id bigint(20) NOT NULL;
