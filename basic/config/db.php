ef201e3bba695e12f9b09171424c1855e7f9fb26
SergioXXI <sergiobpmoralino@hotmail.com>
2024-01-10 20:02:44 +0100

    Merge-Develop

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,18 +4,10 @@
 //return require( 'db_orig.php');
 //return require( 'db_proyecto_01_seguimiento_ligas_deportivas.php');
 //return require( 'db_proyecto_02_reservas_restaurantes.php');
-<<<<<<< HEAD
-//return require( 'db_proyecto_03_organizacion_torneos.php');
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=daw2_2023_03_organizacion_torneos',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
-=======
+
+
 return require( 'db_proyecto_03_organizacion_torneos.php');
->>>>>>> develop
+
 ?>
 <pre>
 Descomenta la línea que se corresponde con tu proyecto de trabajo en el archivo de configuración de la base de datos.
