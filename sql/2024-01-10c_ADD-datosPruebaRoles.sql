f6920decce5cc841e61f60584abb8727098075fe
Manuel Dominguez <121114005+manuDomin@users.noreply.github.com>
2024-01-12 03:38:59 +0100

    Update 2024-01-10c_ADD-datosPruebaRoles.sql
    Modificados datos de roles ya que no estaban actualizados

--- sql/2024-01-10c_ADD-datosPruebaRoles.sql
+++ sql/2024-01-10c_ADD-datosPruebaRoles.sql
@@ -3,14 +3,17 @@ INSERT INTO `auth_item` (`name`, `type`, `description`) VALUES
 ('admin', 1, 'Administrador del sistema'),
 ('organizador', 1, 'Usuario registrado que organiza torneos'),
 ('gestor', 1, 'Usuario registrado que gestiona equipos'),
-('participante', 1, 'Usuario registrado que participa en torneos');
+('usuario', 1, 'Usuario registrado que participa en torneos');
 
 INSERT INTO `auth_assignment` (`item_name`, `user_id`) VALUES 
 ('sysadmin', 1),
 ('admin', 2),
 ('organizador', 3),
 ('gestor', 4),
-('participante', 5);
+('usuario', 5),
+('usuario', 6),
+('usuario', 7),
+('usuario', 8);
 
 INSERT INTO auth_item_child(parent,child) 
 VALUES
