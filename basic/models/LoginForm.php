75c43fc87a511ea4a8adc30ae4a2008a2c629cfb
JorgeApSa <id00800819@usal.es>
2024-01-13 12:49:35 +0100

    Modificacion del formulario de inicio de sesion
    Permite que cuando un usuario sin rol que ha registrado un gestor posteriormente quiere crearse una cuenta de usuario con el mismo correo y contraseña pueda hacerlo sin problemas

--- basic/models/LoginForm.php
+++ basic/models/LoginForm.php
@@ -4,6 +4,7 @@ namespace app\models;
 
 use Yii;
 use yii\base\Model;
+use yii\web\IdentityInterface;
 
 class LoginForm extends Model
 {
@@ -28,6 +29,18 @@ class LoginForm extends Model
             $this->addError('password', 'Contraseña incorrecta.');
         } else {
             Yii::$app->user->login($user);
+
+            // Verificar si el usuario tiene un rol asignado
+            $auth = Yii::$app->authManager;
+            $role = $auth->getRole('usuario'); //Asignamos el rol de usuario
+
+            //Si el usuario que intenta iniciar sesion no tiene rol asignado (usuario que ha creado un gestor sin rol)
+            //Pueda hacerlo con el mismo correo y contraseña que tenia antes
+            if (!$auth->checkAccess($user->id, $role->name)) {
+                // Asignar el rol al usuario
+                $auth->assign($role, $user->id);
+            }
+
             return true;
         }
 
