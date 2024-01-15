174662fb57988def0e05ad8ddb44668d4759df2a
unknown <raftars77@yahoo.com>
2024-01-15 13:57:03 +0100

    Añadida navbar con los apartados de la aplicación
    Se ha añadido y se le han dado algunos estilos, aunque queda retocar el aspecto general de la misma

---
+++ basic/views/layouts/_nav.php
@@ -0,0 +1,118 @@
+<?php 
+
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+use yii\helpers\Html;
+
+/*
+GENERAL - Inicio (todos) | (A = menu administración)
+
+GRUPO 1 - 
+    -> Registro [/user/register] -> Invitado y gestor
+    -> Login [/site/login] -> Invitado
+    -> Logout [/site/logout] -> Todos menos Invitado
+    A<- Roles [/auth-item] -> sysadmin 
+    A<- Usuarios [/user] -> sysadmin, admin
+    -> Perfil [/user/view-profile/] -> Todos menos Invitado
+
+GRUPO 2 -
+    <- Torneos [/torneo/index] -> Todos
+    A<- Reservas [/reserva/index] -> sysadmin, admin, organizador, gestor
+    A<- Disciplina [/disciplinas/index] -> sysadmin, admin
+    A<- Categoria [/categoria/index] -> sysadmin, admin
+    A<- Clase [/clase/index] -> sysadmin, admin
+    A<- Tipo torneo [/tipo-torneo/index] -> sysadmin, admin
+    A<- Premios ['premio/index'] -> sysadmin, admin, organizador
+
+GRUPO 3 - 
+    A<- Equipos ['/equipo/index'] => sysadmin, admin, gestor
+    A<- Participantes ['participante/index'] => sysadmin, admin, gestor
+
+GRUPO 4 - 
+    <- Pistas ['pista/pistas'] -> Todos
+    A<- Pistas ['pista/index'] -> sysadmin, admin
+    A<- Direccion ['direccion/index] -> sysadmin, admin
+    <- Calendario ['calendario/index] -> Todos
+    A<- Backup ['backup/index'] -> sysadmin, admin
+    A<- Logs ['log/index'] -> sysadmin, admin 
+*/
+
+NavBar::begin([
+    'brandLabel' => Yii::$app->name,
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top'],
+    'innerContainerOptions' => ['class' => 'container-fluid px-4'],
+    'containerOptions' => ['class' => 'justify-content-between'],
+]);
+
+$elementos_izquierda = [
+    ['label' => 'Inicio', 'url' => ['/site/index']],
+    ['label' => 'Torneos', 'url' => ['/torneo/index']],
+    ['label' => 'Pistas', 'url' => ['/pista/pistas']],
+    ['label' => 'Calendario', 'url' => ['/calendario/index']],
+    [
+        'label' => 'Panel de administración',
+        'items' => [
+            ['label' => 'Usuarios', 'url' => ['/user/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Reservas', 'url' => ['/reserva/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') ||
+                                                                              Yii::$app->user->can('organizador') || Yii::$app->user->can('gestor')],
+            ['label' => 'Disciplinas', 'url' => ['/disciplina/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Categorias de Torneo', 'url' => ['/categoria/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Clases de Torneo', 'url' => ['/clase/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Tipo de Torneo', 'url' => ['/tipo-torneo/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Premios', 'url' => ['/premio/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') ||
+                                                                            Yii::$app->user->can('organizador')],
+            ['label' => 'Equipos', 'url' => ['/equipo/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') ||
+                                                                            Yii::$app->user->can('gestor')],
+            ['label' => 'Participantes', 'url' => ['/participante/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') ||
+                                                                                        Yii::$app->user->can('gestor')],
+            ['label' => 'Pistas', 'url' => ['/pista/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            /* ['label' => 'Reserva Pistas', 'url' => ['/reserva-pista/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')], */
+            ['label' => 'Direcciones', 'url' => ['/direccion/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Backups base de datos', 'url' => ['/backup/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Logs', 'url' => ['/log/index'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin')],
+            ['label' => 'Roles', 'url' => ['/auth-item/index'], 'visible' => Yii::$app->user->can('sysadmin')],
+        ],
+        'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') || Yii::$app->user->can('organizador') || Yii::$app->user->can('gestor'),
+        'dropdownOptions' => [
+            'class' => 'dropdown-menu-end dropdown-menu-dark',
+        ],
+    ],
+];
+
+//Si el usuario es invitado se le muestra el inicio de sesión
+if(Yii::$app->user->isGuest) {
+    $elementos_derecha = [
+        ['label' => 'Iniciar sesión', 'url' => ['/site/login']],
+        ['label' => 'Registrarse', 'url' => ['/user/register']],
+    ];
+} else {
+    $elementos_derecha = [
+        [
+            'label' => Html::encode(Yii::$app->user->identity->nombre),
+            'items' => [
+                ['label' => 'Perfil', 'url' => ['/user/view-profile']],
+                ['label' => 'Registrar usuario', 'url' => ['/user/register'], 'visible' => Yii::$app->user->can('sysadmin') || Yii::$app->user->can('admin') ||
+                                                                                            Yii::$app->user->can('gestor')],
+                ['label' => 'Logout', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']],
+            ],
+            'dropdownOptions' => [
+                'class' => 'dropdown-menu-end dropdown-menu-dark',
+            ],
+        ],
+    ];
+}
+
+
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav navbar-right'],
+    'items' => $elementos_izquierda,
+]);
+
+
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav ml-auto'],
+    'items' => $elementos_derecha,
+]);
+
+NavBar::end();
--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -7,8 +7,6 @@ use app\assets\AppAsset;
 use app\widgets\Alert;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
 use yii\web\View;
 
 AppAsset::register($this);
@@ -32,59 +30,7 @@ $this->registerJsFile('https://kit.fontawesome.com/6a8d4512ef.js', ['position' =
 <?php $this->beginBody() ?>
 
 <header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            ['label' => 'Test', 'url' => ['/test/index']],
-            ['label' => 'Torneos', 'url' => ['/torneo/index']],
-            ['label' => 'Disciplinas', 'url' => ['/disciplina/index']],
-            ['label' => 'Clases de torneo', 'url' => ['/clase/index']],
-            ['label' => 'Reservas', 'url' => ['/reserva/index']],
-            ['label' => 'Materiales', 'url' => ['/material/index']],
-            ['label' => 'Partidos', 'url' => ['/partido/index']],
-            [
-                'label' => 'Registro',
-                'url' => ['/user/register'],
-                'visible' => Yii::$app->user->isGuest,
-            ],            
-            !Yii::$app->user->isGuest && (Yii::$app->user->can('admin') || Yii::$app->user->can('sysadmin'))
-                ? ['label' => 'Usuarios', 'url' => ['/user']]
-                : '',
-            Yii::$app->user->can('sysadmin')
-                ? ['label' => 'Roles', 'url' => ['/auth-item']]
-                : '',
-            ['label' => 'Pistas', 'url' => ['/pista/pistas']],
-            ['label' => 'Modificar Pistas', 'url' => ['/pista/index']],
-            ['label' => 'Modificar Direccion', 'url' => ['/direccion/index']],
-            ['label' => 'Modificar Reserva Pista', 'url' => ['/reserva-pista/index']],
-            ['label' => 'Modificar Logs', 'url' => ['/log/index']],
-            ['label' => 'Calendario', 'url' => ['/calendario/index']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : ['label' => 'Mi Cuenta', 'url' => ['/user/view-profile/']],
-            Yii::$app->user->isGuest
-                ? ''
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->email . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>',
-        ]
-    ]);
-    NavBar::end();
-    ?>
+    <?= $this->render('_nav'); //Cargar el nav ?> 
 </header>
 
 <main id="main" class="flex-shrink-0" role="main">
